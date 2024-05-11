f541743b6c202f68525bc0e2318366655095824f
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-11 15:38:55 +0800

    Implement useInput custom hooks into multiple inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import SimpleInput from "./components/SampleInput";
+import BasicForm from "./components/BasicForm";
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      <BasicForm />
     </div>
   );
 }
--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -1,22 +1,98 @@
+import useInput from "../hooks/use-input";
+
+const validate = {
+  isNotEmpty: (value) => value.trim() !== "",
+  isEmail: (value) => value.includes("@"),
+};
+
 const BasicForm = (props) => {
+  const { isNotEmpty, isEmail } = validate;
+  const {
+    value: firstNamevalue,
+    isValid: firstNameIsValid,
+    hasError: firstNameError,
+    changeHandler: firstNameHandler,
+    blurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameError,
+    changeHandler: lastNameHandler,
+    blurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailError,
+    changeHandler: emailHandler,
+    blurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+
+  const submitHandler = (e) => {
+    e.preventDefault;
+    if (!firstNameIsValid || !lastNameIsValid || !emailIsValid) return;
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
+  let formValid = false;
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formValid = true;
+  }
+
+  const firstNameClass = firstNameError
+    ? "form-control invalid"
+    : "form-control";
+  const lastNameClass = lastNameError ? "form-control invalid" : "form-control";
+  const emailClass = emailError ? "form-control invalid" : "form-control";
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={firstNameClass}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={firstNamevalue}
+            onChange={firstNameHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameError && (
+            <p className="error-text">First Name is Invalid</p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={lastNameClass}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={lastNameValue}
+            onChange={lastNameHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameError && <p className="error-text">Last Name is Invalid</p>}
         </div>
       </div>
-      <div className="form-control">
+      <div className={emailClass}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={emailValue}
+          onChange={emailHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailError && <p className="error-text">Email is Invalid</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formValid}>Submit</button>
       </div>
     </form>
   );
